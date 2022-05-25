dec4149cfe0d2e5160ede73801698c4a365943a8
Ajay <ajy9582@gmail.com>
2022-05-25 16:38:58 +0530

    added login and signup

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { useState } from "react";
 
-const login = () => {
-  return <div>login</div>;
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+    };
+    console.log(data);
+    // axios
+    //   .post("/auth/user/signup")
+    //   .then((res) => console.log(res))
+    //   .catch((e) => console.log(e));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <label>Enter your password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
-export default login;
+export default Login;
